a970e1bc88bef515817f409c638f1a9af83dba7b
Sambillingham <hello@sambillingham.com>
2022-11-04 22:16:13 +0100

    rmeove: console.logs

--- pages/api/getUsername.js
+++ pages/api/getUsername.js
@@ -16,7 +16,6 @@ export default async function handler(req, res) {
         .eq('address', address)
         .single()
     
-    console.log(data, error)
     if (!error) {
         res.status(200).json( { success: true, username: data.username })
     } else {
